ecb4c41c2356fd04dce80a4187932d31fea3a2fe
Shifa Sheikh <shifasheikh1904@gmail.com>
2025-04-08 00:08:09 +0530

    add store and auth setup

---
+++ frontend/src/components/auth/layout.jsx
@@ -0,0 +1,13 @@
+
+
+function AuthLayout(){
+    return(
+        <div className="flex-min-h-screen w-full">
+            <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
+
+            </div>
+        </div>
+    );
+}
+
+export default AuthLayout;
